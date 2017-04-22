3fabd9a9d0744fea2c0188dcc35c94b7ac77d0f9
xiaowu <wuhao07315@163.com>
2017-04-22 22:52:02 +0800

    Initial commit

---
+++ src/com/xiaowu/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.xiaowu;
+
+/**
+ * Created by Administrator on 2017/4/22 0022.
+ */
+public class HelloWorld {
+    public static void main(String [] args){
+        System.out.println("Hello World");
+    }
+}
